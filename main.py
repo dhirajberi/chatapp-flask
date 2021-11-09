8d2edaa1d9676f52d761a9a38a8d4aebe506bb15
dhirajberi <dhirajberi.official@gmail.com>
2021-11-09 15:07:25 +0530

    username added

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
@@ -10,9 +10,14 @@ def handleMessage(msg):
 	print('Message: ' + msg)
 	send(msg, broadcast=True)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-	return render_template('index.html')
+	if request.method == 'POST':
+		name = request.form.get('name')
+		return render_template('index.html', name=name)
+
+	return render_template('register.html')
+
 
 if __name__ == '__main__':
 	socketio.run(app)
